bbb5b37a96326fef2e41f85b69e87818c233fc15
borntra <borntra@amazon.com>
2025-03-04 09:54:18 +0100

    Added kubernetes provider

---
+++ kubernetes.tf
@@ -0,0 +1,82 @@
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      module.eks.cluster_name
+    ]
+  }
+}
+
+# resource "kubernetes_deployment" "nginx" {
+#   metadata {
+#     name = "scalable-nginx-example"
+#     labels = {
+#       App = "ScalableNginxExample"
+#     }
+#   }
+
+#   spec {
+#     replicas = 2
+#     selector {
+#       match_labels = {
+#         App = "ScalableNginxExample"
+#       }
+#     }
+#     template {
+#       metadata {
+#         labels = {
+#           App = "ScalableNginxExample"
+#         }
+#       }
+#       spec {
+#         container {
+#           image = "nginx:1.7.8"
+#           name  = "example"
+
+#           port {
+#             container_port = 80
+#           }
+
+#           resources {
+#             limits = {
+#               cpu    = "0.5"
+#               memory = "512Mi"
+#             }
+#             requests = {
+#               cpu    = "250m"
+#               memory = "50Mi"
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
+
+# resource "kubernetes_service" "nginx" {
+#   metadata {
+#     name = "nginx-example"
+#   }
+#   spec {
+#     selector = {
+#       App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+#     }
+#     port {
+#       port        = 80
+#       target_port = 80
+#     }
+
+#     type = "LoadBalancer"
+#   }
+# }
+
+# output "lb_ip" {
+#   value = kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.hostname
+# }
+
--- versions.tf
+++ versions.tf
@@ -6,5 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.83"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.16.1"
+    }
   }
 }
