9214c4da54478efb562e601143b6b7133354aa7b
borntra <borntra@amazon.com>
2025-03-04 12:37:08 +0100

    Added deploy scripts alpha

---
+++ deploy.bat
@@ -0,0 +1,5 @@
+@echo off
+
+docker build -t 861567669929.dkr.ecr.us-east-1.amazonaws.com/pool/ms1:latest ./microservice-1
+
+docker push 861567669929.dkr.ecr.us-east-1.amazonaws.com/pool/ms1:latest
