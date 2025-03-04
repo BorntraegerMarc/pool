376c9d07ef0287f8b479f01b0a615d9e0770cc67
borntra <borntra@amazon.com>
2025-03-04 10:25:34 +0100

    Migrated to Terraform managed k8s resources

--- kubernetes.tf
+++ kubernetes.tf
@@ -13,70 +13,115 @@ provider "kubernetes" {
   }
 }
 
-# resource "kubernetes_deployment" "nginx" {
-#   metadata {
-#     name = "scalable-nginx-example"
-#     labels = {
-#       App = "ScalableNginxExample"
-#     }
-#   }
+resource "kubernetes_namespace" "game-2048" {
+  metadata {
+    name = "game-2048"
+    labels = {
+      name = "game-2048"
+    }
+  }
+}
 
-#   spec {
-#     replicas = 2
-#     selector {
-#       match_labels = {
-#         App = "ScalableNginxExample"
-#       }
-#     }
-#     template {
-#       metadata {
-#         labels = {
-#           App = "ScalableNginxExample"
-#         }
-#       }
-#       spec {
-#         container {
-#           image = "nginx:1.7.8"
-#           name  = "example"
+resource "kubernetes_deployment" "deployment-2048" {
+  metadata {
+    name      = "deployment-2048"
+    namespace = "game-2048"
+  }
 
-#           port {
-#             container_port = 80
-#           }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        "app.kubernetes.io/name" = "app-2048"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          "app.kubernetes.io/name" = "app-2048"
+        }
+      }
+      spec {
+        container {
+          image             = "public.ecr.aws/l6m2t8p7/docker-2048:latest"
+          name              = "app-2048"
+          image_pull_policy = "Always"
 
-#           resources {
-#             limits = {
-#               cpu    = "0.5"
-#               memory = "512Mi"
-#             }
-#             requests = {
-#               cpu    = "250m"
-#               memory = "50Mi"
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
+          port {
+            container_port = 80
+          }
 
-# resource "kubernetes_service" "nginx" {
-#   metadata {
-#     name = "nginx-example"
-#   }
-#   spec {
-#     selector = {
-#       App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
-#     }
-#     port {
-#       port        = 80
-#       target_port = 80
-#     }
+          resources {
+            requests = {
+              cpu = "0.5"
+            }
+          }
+        }
+      }
+    }
+  }
+}
 
-#     type = "LoadBalancer"
-#   }
-# }
+resource "kubernetes_service" "service-2048" {
+  metadata {
+    name      = "service-2048"
+    namespace = "game-2048"
+  }
 
-# output "lb_ip" {
-#   value = kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.hostname
-# }
+  spec {
+    port {
+      port        = 80
+      target_port = 80
+      protocol    = "TCP"
+    }
+    type = "NodePort"
+    selector = {
+      "app.kubernetes.io/name" = "app-2048"
+    }
+  }
+}
 
+resource "kubernetes_ingress_class_v1" "alb" {
+  metadata {
+    name = "alb"
+    # namespace = "game-2048"
+    labels = {
+      "app.kubernetes.io/name" = "LoadBalancerController"
+    }
+  }
+
+  spec {
+    controller = "eks.amazonaws.com/alb"
+  }
+}
+
+resource "kubernetes_ingress_v1" "ingress-2048" {
+  metadata {
+    name      = "ingress-2048"
+    namespace = "game-2048"
+    annotations = {
+      "alb.ingress.kubernetes.io/scheme"      = "internet-facing"
+      "alb.ingress.kubernetes.io/target-type" = "ip"
+    }
+  }
+
+  spec {
+    ingress_class_name = "alb"
+    rule {
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "service-2048"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
