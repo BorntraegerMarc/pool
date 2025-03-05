a14a60da4e4b70cceb6b4b9cb827a2f899e3db23
borntra <borntra@amazon.com>
2025-03-05 11:56:41 +0100

    feat: DB Connection

--- microservice-2/main.js
+++ microservice-2/main.js
@@ -1,17 +1,34 @@
 import { GetSecretValueCommand, SecretsManagerClient } from "@aws-sdk/client-secrets-manager";
 import { createServer } from "node:http";
+import pg from "pg";
+const { Client } = pg;
 
-const client = new SecretsManagerClient({ region: process.env.AWS_REGION });
-const command = new GetSecretValueCommand({
+const secretsManagerClient = new SecretsManagerClient({ region: process.env.AWS_REGION });
+const secretsManagerCmd = new GetSecretValueCommand({
   SecretId: process.env.DB_SECRET_ARN,
 });
-const response = await client.send(command);
 
-console.log("DB Secrets: ", response.SecretString);
-const secret = JSON.parse(response.SecretString);
-console.log("DB username: ", secret.username);
-console.log("DB password: ", secret.password);
+console.log("Requesting Secrets Manager");
+const secretsManagerRes = await secretsManagerClient.send(secretsManagerCmd);
+const dbSecrets = JSON.parse(secretsManagerRes.SecretString);
 
+console.log("Init DB Client");
+
+const dbclient = new Client({
+  user: dbSecrets.username,
+  password: dbSecrets.password,
+  database: "pooldb",
+  host: "pooldb.cluster-cak9debc6yeq.us-east-1.rds.amazonaws.com",
+});
+await dbclient.connect();
+
+console.log("DB Connected!!!");
+
+const res2 = await dbclient.query("select * from information_schema.tables;");
+
+console.log("DB Query nr. of rows: ", res2.rowCount);
+
+console.log("Starting server...");
 const server = createServer(async (req, res) => {
   console.log("MS-2 Request received");
   res.writeHead(200, { "Content-Type": "text/plain" });
