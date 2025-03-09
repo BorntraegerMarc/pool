8e2b28511a1d557b89be680527b3d0853605d805
borntra <borntra@amazon.com>
2025-03-09 21:21:57 +0100

    fix: Output incl. web api hostname

--- outputs.tf
+++ outputs.tf
@@ -9,5 +9,5 @@ output "eks_cluster_endpoint" {
 
 output "web_api_hostname" {
   description = "Web API Hostname"
-  value       = "Load Balancer takes some extra time to provision. Please check your endpoint for Web API under https://${local.region}.console.aws.amazon.com/ec2/home?LoadBalancers:v=3&region=${local.region}#LoadBalancers:v=3;"
+  value       = length(kubernetes_ingress_v1.ingress-ms1.status[0].load_balancer[0].ingress) > 0 ? kubernetes_ingress_v1.ingress-ms1.status[0].load_balancer[0].ingress[0].hostname : "The Load Balancer takes more time to provision. Please check your endpoint for the Web API once the load balancer becomes available under https://${local.region}.console.aws.amazon.com/ec2/home?LoadBalancers:v=3&region=${local.region}#LoadBalancers:v=3;"
 }
